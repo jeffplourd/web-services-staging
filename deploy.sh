30694881d51c4eb9ebd73aa96d4ec82b314028e4
jplourd <jeffplourd@gmail.com>
2017-06-14 08:31:11 -0500

    typo

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 # Exit on any error
 set -e
 
-sudo
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment web-services-staging -p '{"spec":{"template":{"spec":{"containers":[{"name":"web-services-staging","image":"us.gcr.io/web-services-staging/hello:'"$CIRCLE_SHA1"'"}]}}}}'
