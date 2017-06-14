b7afb40b7a2fd552b9da33b809314a4607a0b1ae
jplourd <jeffplourd@gmail.com>
2017-06-14 08:50:27 -0500

    adds change ownership to config

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # Exit on any error
 set -e
 
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+sudo chown -R ${USER} /home/ubuntu/.config
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment web-services-staging -p '{"spec":{"template":{"spec":{"containers":[{"name":"web-services-staging","image":"us.gcr.io/web-services-staging/hello:'"$CIRCLE_SHA1"'"}]}}}}'
