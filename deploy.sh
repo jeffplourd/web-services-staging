48c4a8cb6ce1780708fecb29b600f7c4566441d6
jplourd <jeffplourd@gmail.com>
2017-06-14 07:41:38 -0500

    adds circleci config

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+kubectl patch deployment web-services-staging -p '{"spec":{"template":{"spec":{"containers":[{"name":"web-services-staging","image":"us.gcr.io/web-services-staging/hello:'"$CIRCLE_SHA1"'"}]}}}}'
