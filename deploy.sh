77e25b2a2d5deb665fe144dfacb797596367efb0
jplourd <jeffplourd@gmail.com>
2017-06-15 10:14:05 -0500

    removes secret config from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 sudo chown -R ${USER} /home/ubuntu/.config
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=${HOME}/gcloud-service-key.json
-kubectl create secret generic cloudsql-db-credentials --from-literal=username=postgres --from-literal=password=postgres
+#kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=${HOME}/gcloud-service-key.json
+#kubectl create secret generic cloudsql-db-credentials --from-literal=username=postgres --from-literal=password=postgres
 kubectl replace -f ./deployment.yml
 #deployment web-services-deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"web-services-deployment","image":"us.gcr.io/web-services-staging/hello:'"$CIRCLE_SHA1"'"}]}}}}'
