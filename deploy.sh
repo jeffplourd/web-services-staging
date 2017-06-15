9102c073fa4e7fce53d2c1dc52173fa76961bff6
jplourd <jeffplourd@gmail.com>
2017-06-15 09:37:38 -0500

    updates deploy to push docker image

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 # Exit on any error
 set -e
 
-#sudo chown -R ${USER} /home/ubuntu/.config
-#sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
+sudo chown -R ${USER} /home/ubuntu/.config
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 #sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 #kubectl replace -f ./deployment.yml
 #deployment web-services-deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"web-services-deployment","image":"us.gcr.io/web-services-staging/hello:'"$CIRCLE_SHA1"'"}]}}}}'
